d06944d014bbc6eba53668676f098b570fd5c62b
Дмитрий Чибриков <chi-dimochka@yandex.ru>
2021-04-03 21:56:55 +0400

    added some classes

--- MarioV2/Main.cpp
+++ MarioV2/Main.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <vector>
 
-#define g 9.8
+#define g 0.1
+#define TIME_SCALE 8000.0
 
 using namespace sf;
 using namespace std;
@@ -16,11 +18,30 @@ public:
     ~BaseEntity();
     Sprite sprite;
     bool isKinematic;
-    bool onGround;
     float speedx;
     float speedy;
-    void Update(float& time);
+    void Update(double time);
     void Draw(RenderWindow& window);
+
+private:
+
+};
+
+class Game
+{
+public:
+    Game();
+    ~Game();
+
+private:
+
+};
+
+class Player
+{
+public:
+    Player();
+    ~Player();
 
 private:
 
@@ -30,6 +51,12 @@ int main()
 {
     window.setVerticalSyncEnabled(true);
     Clock timeClock;
+    Texture texture;
+    texture.loadFromFile("Mario.png");
+    Sprite sprite(texture);
+    sprite.setScale(2, 2);
+    BaseEntity entity(sprite, Vector2f(0, 0), true);
+
 
     while (window.isOpen())
     {
@@ -40,21 +67,21 @@ int main()
                 window.close();
         }
 
+        entity.Update(timeClock.getElapsedTime().asMicroseconds() / TIME_SCALE);
         timeClock.restart();
-
+        
         window.clear();
+        entity.Draw(window);
         window.display();
     }
 }
 
-void BaseEntity::Update(float& time)
+void BaseEntity::Update(double time)
 {
     if (!isKinematic)
         return;
-    if (onGround)
-        return;
-    speedy = speedy + g * time;
-    sprite.move(0, speedy * time);
+    speedy = speedy - g * time;
+    sprite.move(0, -speedy * time);
 }
 
 void BaseEntity::Draw(RenderWindow& window)
@@ -68,11 +95,29 @@ BaseEntity::BaseEntity()
 
 BaseEntity::BaseEntity(Sprite sprite, Vector2f position, bool isKinematic)
 {
+    speedx = 0;
+    speedy = 0;
     this->sprite = sprite;
     sprite.setPosition(position);
     this->isKinematic = isKinematic;
 }
 
 BaseEntity::~BaseEntity()
+{
+}
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+Player::Player()
+{
+}
+
+Player::~Player()
 {
 }
